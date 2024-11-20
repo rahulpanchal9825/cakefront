0d5832d3551bc1b933cf607112fd7dbf1883af9b
rahulpanchal9825 <rahul007panchal@gmail.com>
2024-11-20 20:11:55 +0530

    vdfvd

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import CaraouselProduct from './component/CaraouselProduct';
+import Description from './component/Description';
+import Navbar from './component/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './component/Home';
+import Login from './component/Login';
+import AddProduct from './component/AddProduct';
+function App() {
+   const [data, setData] = useState([])
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch("https://cakeback-rahuls-projects-44dfd132.vercel.app/api/list-product-all");
+            if (!response.ok) {
+              throw new Error(`Error: ${response.status}`);
+            }
+            const result = await response.json();
+            setData(result);
+          } catch (err) {
+            // setError(err.message);
+          } finally {
+            // setLoading(false);
+          }
+        };
+    
+        fetchData();
+      }, []);
+      console.log("dfdisuhfioh",data?.allProducts)
+    return (
+        <>
+            <Router>
+            <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/:category" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/add" element={<AddProduct />} />
+                </Routes>
+            </Router>
+        </>
+    );
+}
+
+export default App;
